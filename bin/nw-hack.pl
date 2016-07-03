ace54d4b31d37ee3149949b957f896283f218fec
Ivan Wills <Ivan.Wills2@optus.com.au>
2016-07-04 09:48:55 +1000

    Adding documentation

--- bin/nw-hack.pl
+++ bin/nw-hack.pl
@@ -44,7 +44,7 @@ sub main {
         'del',
         'host|h',
         'username|u=s',
-        'config|c',
+        'config|c=s',
         'verbose|v+',
         'man',
         'help',
@@ -130,7 +130,7 @@ __DATA__
 
 =head1 NAME
 
-nw-hack.pl - <One-line description of commands purpose>
+nw-hack.pl - A quick way for setting up temporary network routes/and host mappings
 
 =head1 VERSION
 
@@ -138,74 +138,64 @@ This documentation refers to nw-hack.pl version 0.0.1
 
 =head1 SYNOPSIS
 
-   nw-hack.pl [option]
+   nw-hack.pl [(-d|--device)[=]name]
+   nw-hack.pl --del  [(-d|--device)[=]name]
+   nw-hack.pl --host [--username[=]user] host1 [host2 ...]
 
  OPTIONS:
-  -o --other         other option
+  -d --device[=]str
+                Use this device to route connections over
+     --del      Remove routes and host mappings
+  -h --host     Try to find the IP address of one or more hosts, these will
+                then be added to the config hosts.
+  -u --username[=]str
+                The user to connect as for --host
+  -c --config[=]file
+                Use this file as the config file for nw-hack (Devault ~/.nw-hack.yml)
 
-  -v --verbose       Show more detailed option
-     --version       Prints the version information
-     --help          Prints this help information
-     --man           Prints the full documentation for nw-hack.pl
+  -v --verbose  Show more detailed option
+     --version  Prints the version information
+     --help     Prints this help information
+     --man      Prints the full documentation for nw-hack.pl
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
+=head2 CONFIG
 
-May include numerous subsections (i.e., =head2, =head3, etc.).
+The config file is usually stored in ~/.nw-hack.yml but you can specify
+alternate files via the C<--config>. It is a YAML file where the following keys mean:
+
+=over 4
+
+=item hosts
+
+Specify host/IP addresses to put/remove from the /etc/hosts file
+
+=item routes
+
+Specify IP/network masks to to route traffic the specified device
+
+=item device
+
+The network device to route traffic through (can be overridden with --device)
+
+=item landing
+
+The SSH server to connect to when trying to find IP addresses for hosts
+
+=back
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
-
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
-
-Name the section accordingly.
-
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
-
 =head1 DIAGNOSTICS
-
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
-
 =head1 DEPENDENCIES
-
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
 
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
